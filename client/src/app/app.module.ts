1a43ceda7a82d5c7639b4f2a12aa032160860f12
sai <saibalaka25@gmail.com>
2024-03-01 14:19:58 +0530

    added server

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { ProductsComponent } from './products/products.component';
+import { QuotationComponent } from './quotation/quotation.component';
+import { RequestsComponent } from './requests/requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserComponent,
+    ProductsComponent,
+    QuotationComponent,
+    RequestsComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/quotation/quotation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quotation',
+  templateUrl: './quotation.component.html',
+  styleUrl: './quotation.component.css'
+})
+export class QuotationComponent {
+
+}
